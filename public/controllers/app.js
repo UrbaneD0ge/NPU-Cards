df8378fd4cfde3ad04075df63bf0a274f290d170
Kip Dunlap <kipling.dunlap@gmail.com>
2023-01-30 14:57:00 -0500

    promises again

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -11,26 +11,18 @@ btn.addEventListener('click', (event) => {
     status.innerHTML = 'Geolocation is not supported by your browser';
     return;
   }
-  function success(position) {
+  async function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    // const lat = document.querySelector('#lat');
-    // const lon = document.querySelector('#lon');
-    // lat.value = latitude;
-    // lon.value = longitude;
     status.innerHTML = "Location found: " + latitude + ", " + longitude;
-    return latitude, longitude;
+    await fetch(`https://services5.arcgis.com/5RxyIIJ9boPdptdo/arcgis/rest/services/Official_NPU/FeatureServer/0/query?where=1%3D1&outFields=NAME&geometry=${longitude}%2C${latitude}%2C${longitude}%2C${latitude}&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&returnGeometry=false&outSR=4326&f=json`)
+      .then((data) => {
+        console.log(data.json());
+      })
   }
   function error() {
     status.innerHTML = 'Unable to retrieve your location';
   }
   navigator.geolocation.getCurrentPosition(success, error);
-});
-
-addySearch.addEventListener('click', (event) => {
-  event.preventDefault();
-  fetch(`https://services5.arcgis.com/5RxyIIJ9boPdptdo/arcgis/rest/services/Official_NPU/FeatureServer/?geometry=${latitude},${longitude}&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=*&returnGeometry=false&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=}`)
-    .then((response) => {
-      console.log(response);
-    });
+  return;
 });
